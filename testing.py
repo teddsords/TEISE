b41f58391fa9bbde9cd35d169c966bc3227b9b89
Teddy Ordoñez <teddyonu@hotmail.com>
2021-08-27 08:24:41 -0300

    Initial commit

---
+++ testing.py
@@ -0,0 +1,47 @@
+#%%
+message = 'Hello World!'
+print(message)
+
+# %%
+import gmaps
+import gmaps.datasets
+
+gmaps.configure(api_key = 'AIzaSyCKrR2HGPngLX_1so3aBt017bMTTCoErUc')
+
+earthquake_df = gmaps.datasets.load_dataset_as_df('earthquakes')
+earthquake_df.head()
+# %%
+locations = earthquake_df[['latitude', 'longitude']]
+weights = earthquake_df['magnitude']
+
+fig = gmaps.figure()
+fig.add_layer(gmaps.heatmap_layer(locations, weights = weights))
+fig
+# %%
+gmaps.configure(api_key = 'AIzaSyCKrR2HGPngLX_1so3aBt017bMTTCoErUc')
+
+fig = gmaps.figure()
+fig
+# %%
+gmaps.configure(api_key = 'AIzaSyCKrR2HGPngLX_1so3aBt017bMTTCoErUc')
+
+fig = gmaps.figure(map_type = 'SATELLITE')
+
+#generate some (latitude, longitude) pairs
+
+locations = [(14.1,  -87.2167), (15.5, -88.0)]
+heatmap_layer = gmaps.heatmap_layer(locations)
+fig.add_layer(heatmap_layer)
+fig
+
+# %%
+gmaps.configure(api_key = 'AIzaSyCKrR2HGPngLX_1so3aBt017bMTTCoErUc')
+new_york_coordinates = (40.75, -74.0)
+
+gmaps.figure(center=new_york_coordinates, zoom_level = 12, map_type = 'TERRAIN')
+# %%
+tegus_coordinates = (14.1,  -87.2167)
+gmaps.figure(center = tegus_coordinates, zoom_level = 12, map_type = 'HYBRID')
+# %%
+gmaps.marker(tegus_coordinates)
+# %%
